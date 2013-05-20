8a4dca3ff261970e0a15ec9dbd0cadb8c1142e81
Zeph LaFassett <zephlafassett@zephlafassett-osx.desktop.kink.y>
2013-05-20 13:45:08 -0700

    removed class files

--- gamedev/startest/startest-main/src/com/moral/screens/MainMenuScreen.java
+++ gamedev/startest/startest-main/src/com/moral/screens/MainMenuScreen.java
@@ -1,23 +1,34 @@
 package com.moral.screens;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.moral.startest.StarTest;
 
-public class MainMenuScreen implements Screen 
+public class MainMenuScreen implements Screen
 {
 
 	StarTest game;
-	
+	SpriteBatch spriteBatch;
+	BitmapFont font;
+
 	public MainMenuScreen(StarTest game)
 	{
+		spriteBatch = new SpriteBatch();
+		font = new BitmapFont();
 		this.game = game;
 	}
-	
+
 	@Override
-	public void render(float delta) {
-		// TODO Auto-generated method stub
+	public void render(float delta)
+	{
+		spriteBatch.begin();
+			font.setColor(1.0f, 1.0f, 1.0f, 1.0f);
+			font.draw(spriteBatch, "press z", 10, 30);
+		spriteBatch.end();
+
 		if (Gdx.input.isKeyPressed(Keys.Z))
 		{
 			game.setScreen(game.gameScreen);
@@ -25,39 +36,40 @@ public class MainMenuScreen implements Screen
 	}
 
 	@Override
-	public void resize(int width, int height) {
+	public void resize(int width, int height)
+	{
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void show() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void hide() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void pause() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void resume() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void dispose() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
