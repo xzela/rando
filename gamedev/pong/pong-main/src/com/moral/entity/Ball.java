eb14f1c737f8b578156e2b422b7632cc5809af3c
Zeph LaFassett <zeph@lafassett.com>
2013-07-02 15:39:53 -0700

    ball now bounces

--- gamedev/pong/pong-main/src/com/moral/entity/Ball.java
+++ gamedev/pong/pong-main/src/com/moral/entity/Ball.java
@@ -9,10 +9,16 @@ import com.moral.screens.Board;
 
 public class Ball
 {
-	private float SPEED = 2f;
-	private final static Vector2 RESET_POSITION = new Vector2(5f,5f);
+	private final static float SPEED = 2f;
+	private final static float MAX_SPEED = 10f;
+	private final static float RESET_X = 5f;
+	private final static float RESET_Y = 5f;
+
 	private final static float SIZE = .25f;
 	private Vector2 direction;
+
+	public float ballSpeed;
+	public float getBallSpeed() { return this.ballSpeed; }
 
 	private final Rectangle bounds = new Rectangle();
 	public Rectangle getBounds() { return this.bounds; }
@@ -24,6 +30,7 @@ public class Ball
 
 	public Ball(Vector2 position)
 	{
+		this.ballSpeed = SPEED;
 		this.position = position;
 		this.bounds.setX(this.position.x);
 		this.bounds.setY(this.position.y);
@@ -41,47 +48,67 @@ public class Ball
 	public void update(Paddle player, float delta)
 	{
 
-		this.position.add(this.SPEED * this.direction.x * delta, this.SPEED * this.direction.y * delta);
+		this.position.add(this.ballSpeed * this.direction.x * delta, this.ballSpeed * this.direction.y * delta);
 		this.bounds.setX(this.position.x);
 		this.bounds.setY(this.position.y);
 
-		System.out.println("Paddle bounds: " + player.getBounds());
-		System.out.println("Ball bounds: " + this.getBounds());
+		//System.out.println("Paddle bounds: " + player.getPosition());
+		//System.out.println("Ball bounds: " + this.getBounds());
 
 
 		if (this.bounds.overlaps(player.getBounds()))
 		{
-			float sign = Math.signum(this.position.y - player.getBounds().y);
 			this.direction.x = -this.direction.x;
-			this.direction.y = sign * Math.abs(this.position.y - player.getBounds().y) / 30;
-			this.SPEED += 1;
+
+			float sign = Math.signum(this.position.y + player.getPosition().y);
+			this.direction.y = sign * Math.abs(this.position.y + player.getPosition().y) / 30;
+
+			if (this.ballSpeed < MAX_SPEED)
+			{
+				this.ballSpeed += 2;
+			}
+			if (this.ballSpeed > MAX_SPEED)
+			{
+				this.ballSpeed = MAX_SPEED;
+			}
 		}
+		//System.out.println(this.direction.y);
 		// test to bounce back from wall
-//		if (this.bounds.x >= (Board.BOARD_WIDTH - this.bounds.width))
-//		{
-//			float sign = Math.signum(this.position.y + player.getBounds().y);
-//			this.direction.x = -this.direction.x;
-//			this.direction.y = sign * Math.abs(this.position.y + player.getBounds().y) / 30;
-////			this.SPEED += 1;
-//		}
+		if (this.bounds.x >= (Board.BOARD_WIDTH - this.bounds.width))
+		{
+			float sign = Math.signum(this.position.y);
+			this.direction.x = -this.direction.x;
+			this.direction.y = -this.direction.y;
+			this.direction.y = -sign * Math.abs(this.position.y) / 30;
+		}
 
 		// if ball pasts the left side of the screen.
-		if (this.bounds.x <= 0)
+		if (this.bounds.x < 0)
 		{
+			System.out.println("Ball bounds: " + this.getBounds());
 			this.reset();
 		}
 
-		if (this.bounds.x >= Board.BOARD_WIDTH - this.bounds.width)
+		//System.out.println("Height: " + Board.BOARD_HEIGHT  this.bounds.height);
+		// if ball hits the top of the board
+		if (this.position.y > (Board.BOARD_HEIGHT - this.bounds.height))
 		{
-			this.reset();
+			this.direction.y = -this.direction.y;
 		}
+//
+//		// if ball hits the bottom of the board
+//		if (this.position.y > Board.BOARD_HEIGHT - this.bounds.height)
+//		{
+//			this.direction.y = -this.direction.y;
+//		}
 	}
 
 	private void reset()
 	{
-		this.position = RESET_POSITION;
-		this.bounds.x = this.position.x;
-		this.bounds.y = this.position.y;
+		this.position = new Vector2(RESET_X, RESET_Y);
+		this.ballSpeed = SPEED;
+		this.bounds.setX(this.position.x);
+		this.bounds.setY(this.position.y);
 		this.direction = new Vector2(-1,0);
 	}
 }
