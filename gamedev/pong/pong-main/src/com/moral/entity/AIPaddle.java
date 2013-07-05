bca6b642e4648b493e364ab40c582185f3f8f75c
Zeph Lafassett <zeph@lafassett.com>
2013-07-05 12:56:44 -0700

    modified AI behavior

--- gamedev/pong/pong-main/src/com/moral/entity/AIPaddle.java
+++ gamedev/pong/pong-main/src/com/moral/entity/AIPaddle.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.math.Vector2;
 public class AIPaddle extends Paddle
 {
 
-	private float multi = 1;
+	private final float multi = 1;
 
 	public AIPaddle(Vector2 position)
 	{
@@ -16,19 +16,19 @@ public class AIPaddle extends Paddle
 
 	public void update(Ball ball, float delta)
 	{
-//		this.getPosition().x = ball.getPosition().x;
+		//		this.getPosition().x = ball.getPosition().x;
 		if (ball.getDirection().x > 0)
 		{
 			float dir = Math.signum(ball.getPosition().y - (this.getBounds().y / 2));
 			this.getPosition().y += dir * delta * (ball.ballSpeed * this.multi);
-//			if (dir > 0 && this.getPosition().y > ball.getPosition().y)
-//			{
-//				this.setY(ball.getPosition().y);
-//			}
-//			if (dir < 0 && this.getPosition().y < ball.getPosition().y)
-//			{
-//				this.setY(ball.getPosition().y);
-//			}
+			if (dir > 0 && this.getPosition().y > ball.getPosition().y)
+			{
+				this.setY(ball.getPosition().y);
+			}
+			if (dir < 0 && this.getPosition().y < ball.getPosition().y)
+			{
+				this.setY(ball.getPosition().y);
+			}
 
 		}
 
